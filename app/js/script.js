9a4b79ea39bdb63072de6c86b32d137936ca3df6
ntt-vulk <vu.lk@nttnetworks.com>
2021-01-17 09:36:17 +0700

    hamburger animation fade in - fade out with js

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,16 @@
-console.log('Hello Me');
+const btnHamburger = document.querySelector('#js-btnHamburger');
+const header = document.querySelector('.js-header');
+const overlay = document.querySelector('.js-overlay');
+
+btnHamburger.addEventListener('click', function(){
+	console.log('on click hamburger');
+	if(header.classList.contains('open')){
+		header.classList.remove('open');
+		overlay.classList.add('fade-out');
+		overlay.classList.remove('fade-in');
+	}else{
+		header.classList.add('open');
+		overlay.classList.add('fade-in');
+		overlay.classList.remove('fade-out');
+	}
+})
