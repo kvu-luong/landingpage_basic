821d18d72054f40b9a0840c20c1f407fd76fa4b1
ntt-vulk <vu.lk@nttnetworks.com>
2021-01-17 10:08:33 +0700

    add menu mobile , fix error scroll and handle fade in together with overlay

--- app/js/script.js
+++ app/js/script.js
@@ -1,16 +1,26 @@
 const btnHamburger = document.querySelector('#js-btnHamburger');
+const body = document.querySelector('body');
 const header = document.querySelector('.js-header');
-const overlay = document.querySelector('.js-overlay');
+// const overlay = document.querySelector('.js-overlay');
+const fadeElems = document.querySelectorAll('.js-has-fade');
 
 btnHamburger.addEventListener('click', function(){
 	console.log('on click hamburger');
 	if(header.classList.contains('open')){
+		body.classList.remove('noscroll');
 		header.classList.remove('open');
-		overlay.classList.add('fade-out');
-		overlay.classList.remove('fade-in');
+		fadeElems.forEach(function(element){
+			element.classList.add('fade-out');
+			element.classList.remove('fade-in');
+		})
+		
 	}else{
+		body.classList.add('noscroll');
 		header.classList.add('open');
-		overlay.classList.add('fade-in');
-		overlay.classList.remove('fade-out');
+		fadeElems.forEach(function(element){
+			element.classList.add('fade-in');
+			element.classList.remove('fade-out');
+		})
+		
 	}
 })
